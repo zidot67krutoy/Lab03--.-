f5d9d3ebc10a37181dbc9ab857a38e89e2a9c1a4
zidot67krutoy <kanaksnakqkmem@yandex.ru>
2026-09-11 10:00:49 +0300

    Lab03: logical operators and short-circuit evaluation

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Reflection.Metadata;
+
 Console.WriteLine("Банковский счёт");
 
 double balance = 1000;
@@ -37,3 +39,54 @@ Console.WriteLine($"Попытка №{++attempts}");
 Console.WriteLine($"Всего попыток: {attempts}");
 
 
+Console.WriteLine();
+Console.WriteLine("Операторы сравнения");
+
+double myGrade = 4.6;
+double passingGrade = 4.0;
+int myAge = 20;
+int votingAge = 18;
+bool isPassing = myGrade >= passingGrade;
+bool isExactAge = myAge == votingAge;
+bool canVote = myAge >= votingAge;
+bool isNotFailing = myGrade != 2.0;
+
+Console.WriteLine($"Балл {myGrade} >= {passingGrade}:{isPassing}");
+Console.WriteLine($"Возраст {myAge} == {votingAge}: {isExactAge}");
+Console.WriteLine($"Возраст {myAge} >= {votingAge} (может голосовать): {canVote}");
+Console.WriteLine($"Балл {myGrade} != 2.0 (не двойка): {isNotFailing}");
+
+
+Console.WriteLine();
+Console.WriteLine("Логические операторы");
+
+bool hasPassingGrade = true;
+bool hasAttendance = false;
+bool hasDebt = true;
+
+bool canGetScholarship = hasPassingGrade && hasAttendance;
+bool canRetakeExam = hasPassingGrade || hasAttendance;
+bool isDebtFree = !hasDebt;
+
+Console.WriteLine($"Может получить стипендию (оценка И посещаемость): {canGetScholarship}");
+Console.WriteLine($"Может пересдать(оценка ИЛИ посещаемость): {canRetakeExam}");
+Console.WriteLine($"Нет долгов: {isDebtFree}");
+
+
+Console.WriteLine();
+Console.WriteLine("Короткое замыкание");
+
+bool CheckAndPrint(string label, bool value)
+{
+    Console.WriteLine($"   Вычисляется:  {label}");
+    return value;
+}
+
+Console.WriteLine("Проверяем && (первый операнд false):");
+bool resultAnd = CheckAndPrint("А", false) && CheckAndPrint("B", true);
+Console.WriteLine($"Результат: {resultAnd}");
+
+Console.WriteLine();
+Console.WriteLine("Проверяем || (первый операнд true):");
+bool resultOr = CheckAndPrint("C", true) || CheckAndPrint("D", false);
+Console.WriteLine($"Результат: {resultOr}");
